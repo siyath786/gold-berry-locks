2cc38f54dc56f424649915802deec38acb356e5f
siyath <siyathmaheen6@gmail.com>
2026-01-18 11:44:12 +0530

    all image optimize

--- two.js
+++ two.js
@@ -6,3 +6,8 @@ bar.addEventListener("click", () => {
   bar.children[1].classList.toggle("arrow-2");
   bar.children[2].classList.toggle("arrow-3");
 });
+let butt90 = document.querySelector(".butt90");
+let hellobro = document.querySelector(".hellobro");
+butt90.addEventListener("click", function () {
+  hellobro.classList.toggle("no");
+});
